ca7beb9fcc156864f1dd95fef2cb550417f9da80
Grang404 <chunter404@proton.me>
2025-01-23 13:36:30 +0000

    update to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# TODO: ignore install.sh and READ.me
+# TODO: fix the autosuggestions and shizz
+
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Print colored output
 print_msg() {
@@ -38,6 +44,7 @@ install_packages() {
 	print_msg "Installing base packages..."
 	pacman -S --needed --noconfirm \
 		base base-devel \
+		xorg \
 		ly \
 		awesome \
 		zsh \
@@ -114,59 +121,68 @@ enable_services() {
 	systemctl enable NetworkManager
 	systemctl enable ly.service
 	systemctl enable polkit.service
-
 }
 
+# Create $HOME/.config if not exists and move dotfiles
 move_dotfiles() {
 	print_msg "Moving config files..."
-	SOURCE_DIR="$(pwd)"
-	CONFIG_DIR="$HOME/.config"
-	AWESOME_THEME_DIR="$CONFIG_DIR/awesome/themes"
+
+	# Use the home directory of the user who ran sudo
+	USER_HOME=$(eval echo ~"$SUDO_USER")
+	CONFIG_DIR="$USER_HOME/.config"
+	AWESOMES_DIR="$CONFIG_DIR/awesome/themes"
+
+	# Create config directory if it doesn't exist
+	mkdir -p "$CONFIG_DIR"
 
 	dirs_to_move_to_config=("kitty" "picom" "btop" "nvim" "ranger" "vesktop" "autostart" "dotfiles.sh" "gtk-3.0" "oh-my-zsh" "README.md" "awesome" "fastfetch" "install.sh" "rofi")
 
-	# Move dot files to $HOME/.config
+	# Move dotfiles
 	for dir in "${dirs_to_move_to_config[@]}"; do
-		if [ -d "$SOURCE_DIR/$dir" ]; then
-			mv "$SOURCE_DIR/$dir" "$CONFIG_DIR"
+		SOURCE_PATH="$SCRIPT_DIR/$dir"
+		if [ -e "$SOURCE_PATH" ]; then
+			if [ -d "$SOURCE_PATH" ]; then
+				cp -r "$SOURCE_PATH" "$CONFIG_DIR/"
+			else
+				cp "$SOURCE_PATH" "$CONFIG_DIR/"
+			fi
+			chown -R "$SUDO_USER:$SUDO_USER" "$CONFIG_DIR/$dir"
 			echo "Moved $dir to $CONFIG_DIR"
 		else
-			echo "$dir not found, skipping..."
+			print_error "$dir not found in $SCRIPT_DIR, skipping..."
 		fi
 	done
 
-	# Move .zshrc and .p10k.zsh to $HOME
-	if [ -f "$SOURCE_DIR/.zshrc" ]; then
-		mv "$SOURCE_DIR/.zshrc" "$HOME"
-		echo "Moved .zshrc to $HOME"
-	else
-		echo ".zshrc not found, skipping..."
-	fi
+	# Move .zshrc and .p10k.zsh to $USER_HOME
+	for dotfile in ".zshrc" ".p10k.zsh"; do
+		SOURCE_PATH="$SCRIPT_DIR/$dotfile"
+		if [ -f "$SOURCE_PATH" ]; then
+			cp "$SOURCE_PATH" "$USER_HOME/"
+			chown "$SUDO_USER:$SUDO_USER" "$USER_HOME/$dotfile"
+			echo "Moved $dotfile to $USER_HOME"
+		else
+			print_error "$dotfile not found in $SCRIPT_DIR, skipping..."
+		fi
+	done
 
-	if [ -f "$SOURCE_DIR/.p10k.zsh" ]; then
-		mv "$SOURCE_DIR/.p10k.zsh" "$HOME"
-		echo "Moved .p10k.zsh to $HOME"
+	# Move samurai theme
+	SAMURAI_SOURCE="$SCRIPT_DIR/samurai_wallpaper.png"
+	if [ -d "$SAMURAI_SOURCE" ]; then
+		mkdir -p "$AWESOMES_DIR"
+		cp "$SAMURAI_SOURCE" "$AWESOMES_DIR/"
+		chown -R "$SUDO_USER:$SUDO_USER" "$AWESOMES_DIR/samurai_wallpaper.png"
+		print_msg "Moved samurai_wallpaper.png to $AWESOMES_DIR"
 	else
-		echo ".p10k.zsh not found, skipping..."
+		print_error "Samurai theme not found in $SCRIPT_DIR, skipping..."
 	fi
-
-	# Move samurai to .config/awesome/themes
-	if [ -d "$SOURCE_DIR/samurai" ]; then
-		mkdir -p "$AWESOME_THEME_DIR"
-		mv "$SOURCE_DIR/samurai" "$AWESOME_THEME_DIR"
-		echo "Moved samurai to $AWESOME_THEME_DIR"
-	else
-		echo "samurai directory not found, skipping..."
-	fi
-
 }
 
 install_extras() {
-
+	print_msg "Installing extras..."
+	sudo -u "$SUDO_USER" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$USER_HOME/.config/oh-my-zsh/themes/powerlevel10k"
+	sudo -u "$SUDO_USER" git clone https://github.com/zsh-users/zsh-autosuggestions "$USER_HOME/.config/oh-my-zsh/plugins/"
+	sudo -u "$SUDO_USER" git clone https://github.com/marlonrichert/zsh-autocomplete.git "$USER_HOME/.config/oh-my-zsh/plugins/"
 	sudo -u "$SUDO_USER" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	sudo -u "$SUDO_USER" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/custom}/themes/powerlevel10k
-	sudo -u "$SUDO_USER" git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.config/oh-my-zsh/plugins/"
-	sudo -u "$SUDO_USER" git clone https://github.com/marlonrichert/zsh-autocomplete.git "$HOME/.config/oh-my-zsh/plugins/"
 }
 
 # Main installation flow
