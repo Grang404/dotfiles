77b83cbfe3acc9b2d18771b696534dccfc9c0e90
Grang404 <chunter404@proton.me>
2025-04-25 16:00:00 +0100

    Auto-commit: 2025-04-25 16:00:00

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -508,51 +508,6 @@ require("lazy").setup({
 			})
 		end,
 	},
-	-- { -- Autoformat
-	-- 	"stevearc/conform.nvim",
-	-- 	event = { "BufWritePre" },
-	-- 	cmd = { "ConformInfo" },
-	-- 	keys = {
-	-- 		{
-	-- 			"<leader>F",
-	-- 			function()
-	-- 				require("conform").format({ async = true, lsp_format = "fallback" })
-	-- 			end,
-	-- 			mode = "",
-	-- 			desc = "[F]ormat buffer",
-	-- 		},
-	-- 	},
-	-- 	opts = {
-	-- 		notify_on_error = false,
-	-- 		format_on_save = function(bufnr)
-	-- 			-- Disable "format_on_save lsp_fallback" for languages that don't
-	-- 			-- have a well standardized coding style. You can add additional
-	-- 			-- languages here or re-enable it for the disabled ones.
-	-- 			local disable_filetypes = { c = true, cpp = true }
-	-- 			local lsp_format_opt
-	-- 			if disable_filetypes[vim.bo[bufnr].filetype] then
-	-- 				lsp_format_opt = "never"
-	-- 			else
-	-- 				lsp_format_opt = "fallback"
-	-- 			end
-	-- 			return {
-	-- 				timeout_ms = 500,
-	-- 				lsp_format = lsp_format_opt,
-	-- 			}
-	-- 		end,
-	-- 		formatters_by_ft = {
-	-- 			lua = { "stylua" },
-	-- 			-- Conform can also run multiple formatters sequentially
-	-- 			python = { "black" },
-	-- 			sh = { "shfmt" },
-	-- 			-- You can use 'stop_after_first' to run the first available formatter from the list
-	-- 			javascript = { "prettierd", "prettier", stop_after_first = true },
-	-- 			html = { "prettier" },
-	-- 			htmldjango = { "prettier" },
-	-- 		},
-	-- 	},
-	-- },
-
 	{ -- Autoformat
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
@@ -569,17 +524,62 @@ require("lazy").setup({
 		},
 		opts = {
 			notify_on_error = false,
-			format_on_save = false, -- Completely disable autoformat on save
+			format_on_save = function(bufnr)
+				-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- have a well standardized coding style. You can add additional
+				-- languages here or re-enable it for the disabled ones.
+				local disable_filetypes = { c = true, cpp = true }
+				local lsp_format_opt
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					lsp_format_opt = "never"
+				else
+					lsp_format_opt = "fallback"
+				end
+				return {
+					timeout_ms = 500,
+					lsp_format = lsp_format_opt,
+				}
+			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
 				python = { "black" },
 				sh = { "shfmt" },
+				-- You can use 'stop_after_first' to run the first available formatter from the list
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				html = { "prettier" },
 				htmldjango = { "prettier" },
 			},
 		},
 	},
+
+	-- { -- Autoformat
+	-- 	"stevearc/conform.nvim",
+	-- 	event = { "BufWritePre" },
+	-- 	cmd = { "ConformInfo" },
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>F",
+	-- 			function()
+	-- 				require("conform").format({ async = true, lsp_format = "fallback" })
+	-- 			end,
+	-- 			mode = "",
+	-- 			desc = "[F]ormat buffer",
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		notify_on_error = false,
+	-- 		format_on_save = false, -- Completely disable autoformat on save
+	-- 		formatters_by_ft = {
+	-- 			lua = { "stylua" },
+	-- 			python = { "black" },
+	-- 			sh = { "shfmt" },
+	-- 			javascript = { "prettierd", "prettier", stop_after_first = true },
+	-- 			html = { "prettier" },
+	-- 			htmldjango = { "prettier" },
+	-- 		},
+	-- 	},
+	-- },
 
 	{ -- Autocompletion
 		"hrsh7th/nvim-cmp",
