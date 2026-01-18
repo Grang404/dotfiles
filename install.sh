84db3659f16e8b3c3562fcedb326849ec86990bd
Grang404 <chunter404@proton.me>
2026-01-18 03:52:52 +0000

    uwu

--- install.sh
+++ install.sh
@@ -300,10 +300,9 @@ enable_services() {
 	local services=("${system_services[@]}")
 	[[ "$PROFILE" == "laptop" ]] && services+=("${laptop_services[@]}")
 
+	systemctl mask systemd-networkd.service systemd-networkd.socket systemd-networkd-varlink.socket systemd-networkd-resolve-hook.socket
 	if [[ "$PROFILE" == "laptop" ]]; then
 		systemctl mask systemd-rfkill.service systemd-rfkill.socket
-	else
-		systemctl mask systemd-networkd.service
 
 	fi
 
