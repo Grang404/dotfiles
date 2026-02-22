672a1986c6a02c7b3e339e70a7ca49316d4d77db
Grang404 <chunter404@proton.me>
2026-02-22 09:15:50 +0000

    cooke

--- dots/waybar/scripts/vpn.sh
+++ dots/waybar/scripts/vpn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-if ip link show tun0 up &>/dev/null; then
+if ip link show tun0 &>/dev/null; then
 	echo '{"text":"","class":"connected"}'
 else
 	echo '{"text":"","class":"disconnected"}'
